507deb114a3ca18185ac2d202f6fcf2c3f5e3733
hshse <yakov.petryk@gmail.com>
2021-12-15 22:25:05 +0200

    first handmade slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  const images = [
+  const imagesArr = [
     "./img/1.jpg",
     "./img/2.jpg",
     "./img/3.jpg",
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
     "./img/8.jpg",
     "./img/9.jpg",
   ];
-  const dotsImages = ["./img/0.svg", "./img/1.svg"];
+  const dotsImagesArr = ["./img/0.svg", "./img/1.svg"];
 
   const imgContainer = document.querySelector(".img-container");
   const forwardBtn = document.querySelector(".forward-arrow");
@@ -18,76 +18,89 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const dotsContainer = document.querySelector(".dots");
   let currentSlide = 0;
 
-  function appearImg(img) {
+  function appearImg(arr) {
     const fragment = document.createDocumentFragment();
-    backBtn.classList.add("hide");
-    img.forEach((element) => {
+    arr.forEach((element) => {
       let imgElement = document.createElement("img");
+      imgElement.classList.add("slide-img");
       imgElement.setAttribute("alt", "slider image");
-      imgElement.dataset.slide = "";
       imgElement.setAttribute("src", element);
       fragment.append(imgElement);
     });
     imgContainer.append(fragment);
   }
 
-  function appearDots(dotsContainer, dotsImages) {
-    for (let i = 0; i < images.length; i++) {
+  function appearDots(container, arr) {
+    for (let i = 0; i < imagesArr.length; i++) {
       const liElement = document.createElement("li");
       const dotImage = document.createElement("img");
+      dotImage.classList.add("dot-img");
       dotImage.setAttribute("alt", "dot");
+      dotImage.dataset.slideNum = `${i}`;
       if (i === 0) {
-        dotImage.setAttribute("src", dotsImages[1]);
+        dotImage.setAttribute("src", arr[1]);
       } else {
-        dotImage.setAttribute("src", dotsImages[0]);
+        dotImage.setAttribute("src", arr[0]);
       }
       liElement.append(dotImage);
-      dotsContainer.append(liElement);
+      container.append(liElement);
     }
   }
 
   function hideImg(index) {
-    const images = document.querySelectorAll("img[data-slide]");
-    images.forEach((element) => {
+    const images = document.querySelectorAll(".slide-img");
+    Array.from(images).forEach((element) => {
       element.classList.add("hide");
     });
     images[index].classList.remove("hide");
   }
 
-  function changeDotSrc(slide, dotsImg) {
-    let dotsElements = dotsContainer.querySelectorAll("img");
+  function changeDotSrc(slideNum, arr) {
+    let dotsElements = dotsContainer.querySelectorAll(".dot-img");
     if (dotsElements) {
       Array.from(dotsElements).forEach((element) => {
-        element.setAttribute("src", dotsImg[0]);
+        element.setAttribute("src", arr[0]);
       });
-      dotsElements[slide].setAttribute("src", dotsImg[1]);
+      dotsElements[slideNum].setAttribute("src", arr[1]);
     }
   }
 
-  forwardBtn.addEventListener("click", (e) => {
-    if (currentSlide === images.length - 2) {
-      hideImg(++currentSlide);
+  function checkSlideNum(slideNum) {
+    if (slideNum > imagesArr.length - 2) {
       forwardBtn.classList.add("hide");
-    } else {
       if (backBtn.classList.contains("hide")) backBtn.classList.remove("hide");
-      hideImg(++currentSlide);
-    }
-    changeDotSrc(currentSlide, dotsImages);
-  });
-
-  backBtn.addEventListener("click", (e) => {
-    if (currentSlide === 1) {
-      hideImg(--currentSlide);
+    } else if (slideNum < 1) {
       backBtn.classList.add("hide");
+      if (forwardBtn.classList.contains("hide")) forwardBtn.classList.remove("hide");
     } else {
-      if (forwardBtn.classList.contains("hide"))
-        forwardBtn.classList.remove("hide");
-      hideImg(--currentSlide);
+      forwardBtn.classList.remove("hide");
+      backBtn.classList.remove("hide");
     }
-    changeDotSrc(currentSlide, dotsImages);
+  }
+
+  imgContainer.addEventListener("click", ({ target }) => {
+    if (target.closest(".forward-arrow")) {
+      hideImg(++currentSlide);
+      changeDotSrc(currentSlide, dotsImagesArr);
+      checkSlideNum(currentSlide);
+    }
+
+    if (target.closest(".back-arrow")) {
+      hideImg(--currentSlide);
+      changeDotSrc(currentSlide, dotsImagesArr);
+      checkSlideNum(currentSlide);
+    }
+    
+    if (target.closest(".dots img")) {
+      let slideNumber = Number(target.dataset.slideNum);
+      currentSlide = slideNumber;
+      checkSlideNum(currentSlide);
+      hideImg(currentSlide);
+      changeDotSrc(currentSlide, dotsImagesArr);
+    }
   });
 
-  appearImg(images);
-  appearDots(dotsContainer, dotsImages);
+  appearImg(imagesArr);
+  appearDots(dotsContainer, dotsImagesArr);
   hideImg(currentSlide);
 });
